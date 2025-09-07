9f22801580729a4c397d8b788b4159aff38f4104
emam04bd <emam04bd@gmail.com>
2025-09-07 13:21:07 +0600

    complete exercise 6-11
    - Lesson: Loops and Arrays

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,7 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if(!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || a < 0) return 'ERROR';
+    if(a > b) [a, b] = [b, a];
+    return ((b * (b + 1)) / 2) - ((a * (a - 1)) / 2);
 };
 
 // Do not edit below this line
